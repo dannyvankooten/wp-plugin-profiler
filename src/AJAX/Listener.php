958ce785bf260106103907342dabaac847c0de6d
Danny van Kooten <dannyvankooten@gmail.com>
2015-01-29 16:53:58 +0700

    Clear any possible output messing with JSON response.

--- src/AJAX/Listener.php
+++ src/AJAX/Listener.php
@@ -21,6 +21,11 @@ class Listener {
 		$profiler = new Profiler();
 		$profiler->run();
 
+		// clear output, some plugins might have thrown errors by now.
+		if( ob_get_level() > 0 ) {
+			ob_end_clean();
+		}
+
 		wp_send_json( $profiler->results );
 		exit;
 	}
